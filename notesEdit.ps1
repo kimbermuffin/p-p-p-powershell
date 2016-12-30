4461f55d7f3d81d0f44e775d01d58b1a2a2810f3
kimbermuffin <kimberlyj.duke@gmail.com>
2016-12-30 13:40:08 -0700

    Find the service account.

--- notesEdit.ps1
+++ notesEdit.ps1
@@ -1,6 +1,16 @@
-﻿
+﻿# For better user management
+#
+# kimber duke 2k16
+#
+
+$ticket = Read-Host -Prompt 'Enter ticket reference number'
+$wave = Read-Host -Prompt 'Enter class and wave'
+$admin = Read-Host -Prompt 'Enter sysadmin initials'
+
+$date = Get-date
+
 Import-CSV "C:\scripts\unames.csv" | ForEach-Object { 
 $User = $_.UserName 
  $oldnotes = Get-ADUser $User -Properties info | %{ $_.info}
- Set-ADUser $User -Replace @{info="$($oldnotes)`nNewTextGoesHere"}
+ Set-ADUser $User -Replace @{info="$($oldnotes)`n$ticket - $wave - $admin - $date"}
 }
