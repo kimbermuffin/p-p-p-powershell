3da4cc922c73d001cbedfdcbb783c10187877718
kimbermuffin <kimberlyj.duke@gmail.com>
2016-12-29 17:09:11 -0700

    A notes editor for user changes

---
+++ notesEdit.ps1
@@ -0,0 +1,6 @@
+﻿
+Import-CSV "C:\scripts\unames.csv" | ForEach-Object { 
+$User = $_.UserName 
+ $oldnotes = Get-ADUser $User -Properties info | %{ $_.info}
+ Set-ADUser $User -Replace @{info="$($oldnotes)`nNewTextGoesHere"}
+}
