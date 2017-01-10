86f4a38df449f4b3edf8bab57d9729a757f20ebc
kimbermuffin <kimberlyj.duke@gmail.com>
2017-01-10 12:40:27 -0700

    New script!
    Incrementally checks for username.

---
+++ countauser.ps1
@@ -0,0 +1,21 @@
+﻿$Name = "kduke"
+$dname = $name
+$count = 1
+
+While ($count -le 10) {
+
+        If($User = Get-ADObject -LDAPFilter "(sAMAccountName=$Name)"){
+        Write-Host $User
+
+        Write-Host "Checked $name"
+        
+        $Name = $name
+        $Name = $dname + $count++
+        
+        Write-Host "We have incremented to $Name"
+
+        }
+        else {
+        Write-Host "Make $Name the user!";break}
+     }
+
